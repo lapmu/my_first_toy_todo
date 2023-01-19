e6dbccee886a3b0412da33736a41c9c90d265f53
lapmu <antjdgkdl@gmail.com>
2023-01-19 15:33:27 +0900

    bundle test

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                use: {
+                    loader:'babel-loader',
+                    options: {
+                        presets: [
+                            "@babel/preset-env", ["@babel/preset-react", {"runtime": "automatic"}]
+                        ]
+                    }
+                }
+                
+            },
+            {
+                test: /\.css$/,
+                use : ["style-loader", "css-loader"],
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html")
+    })],
+    mode: 'production'
+}
